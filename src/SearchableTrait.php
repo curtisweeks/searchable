f4bbd6147f46248ba2adb98d522d34b0a950b745
Curtis Weeks <curtis@laffertyequipment.com>
2018-04-16 15:31:21 -0500

    updated namespace for v2

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -1,4 +1,4 @@
-<?php namespace Nicolaslopezj\Searchable;
+<?php namespace Jabbtech\Searchable;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Expression;
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 
 /**
  * Trait SearchableTrait
- * @package Nicolaslopezj\Searchable
+ * @package Jabbtech\Searchable
  * @property array $searchable
  * @property string $table
  * @property string $primaryKey
